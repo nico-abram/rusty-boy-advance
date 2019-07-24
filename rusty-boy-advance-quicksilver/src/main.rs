6c9a4ad924ca95a3b9d522cd1474ac29b75b7eba
Nicolas <abramlujan@gmail.com>
2019-07-24 01:58:55 -0300

    Add file selector to quicksilver

--- rusty-boy-advance-quicksilver/src/main.rs
+++ rusty-boy-advance-quicksilver/src/main.rs
@@ -11,6 +11,19 @@ use quicksilver::{
   Result,
 };
 
+use std::{cell::RefCell, rc::Rc};
+
+#[cfg(target_arch = "wasm32")]
+use stdweb::{
+  unstable::TryInto,
+  web::{
+    document,
+    event::{ChangeEvent, LoadEndEvent},
+    html_element::InputElement,
+    IElement, IEventTarget, INode,
+  },
+};
+
 fn polymorphic_print(x: &str) {
   #[cfg(target_arch = "wasm32")]
   {
@@ -25,7 +38,7 @@ fn polymorphic_print(x: &str) {
 }
 
 struct GameState {
-  gba: GBABox,
+  gba: Rc<RefCell<GBABox>>,
 }
 
 impl State for GameState {
@@ -35,7 +48,7 @@ impl State for GameState {
 
   fn draw(&mut self, window: &mut Window) -> Result<()> {
     let kb = window.keyboard();
-    let gba = &mut self.gba;
+    let mut gba = self.gba.borrow_mut();
     if kb[Key::Left].is_down() {
       gba.input(GBAButton::Left);
     }
@@ -55,12 +68,11 @@ impl State for GameState {
       format!("Rusty Boy Advance Quicksilver ({:.2} FPS)", window.average_fps()).as_str(),
     );
     window.clear(Color::WHITE)?;
-    self
-      .gba
+    gba
       .run_one_frame()
       .map_err(|_| quicksilver::Error::ContextError(String::from("Error running a GBA frame")))?;
     let mut rgba_formatted = [255u8; 255 * 160 * 4];
-    for (output, input) in rgba_formatted.chunks_mut(4).zip(self.gba.video_output().chunks(3)) {
+    for (output, input) in rgba_formatted.chunks_mut(4).zip(gba.video_output().chunks(3)) {
       output[..3].clone_from_slice(input);
     }
     let img = Image::from_raw(&rgba_formatted[..], 240, 160, PixelFormat::RGBA)?;
@@ -72,11 +84,39 @@ impl State for GameState {
   }
 }
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
-  let mut gba = GBABox::new(LogLevel::None, None, Some(polymorphic_print));
+  let gba = Rc::new(RefCell::new(GBABox::new(LogLevel::None, None, Some(polymorphic_print))));
   let rom_bytes = include_bytes!("armwrestler.gba");
   gba
+    .borrow_mut()
     .load(rom_bytes)
     .map_err(|_| quicksilver::Error::ContextError(String::from("Error loading ROM")))?;
+  #[cfg(target_arch = "wasm32")]
+  {
+    let gba = Rc::clone(&gba);
+    let document = document();
+    let newline = document.create_element("br").unwrap();
+    let input: InputElement = document.create_element("input").unwrap().try_into().unwrap();
+    input.set_attribute("type", "file").unwrap();
+    let body = document.body().unwrap();
+    body.append_child(&input);
+    body.append_child(&newline);
+    let input_clone = input.clone();
+    input_clone.add_event_listener(move |_event: ChangeEvent| {
+      let files: stdweb::web::FileList = js!( return @{input.as_ref()}.files; ).try_into().unwrap();
+      let mut files = files.iter();
+      if let Some(file) = files.next() {
+        let reader = stdweb::web::FileReader::new();
+        js!( return @{reader.as_ref()}.readAsArrayBuffer( @{file.as_ref()} ));
+        let gba = Rc::clone(&gba);
+        reader.add_event_listener(move |event: LoadEndEvent| {
+          let data: stdweb::web::ArrayBuffer =
+            js!(return @{event}.target.result;).try_into().unwrap();
+          let data: Vec<_> = data.into();
+          gba.borrow_mut().load(&data[..]).map_err(|_| js!(alert("Invalid ROM");));
+        });
+      }
+    });
+  }
   quicksilver::lifecycle::run_with(
     "Rusty Boy Advance Quicksilver",
     Vector::new(240, 160),
